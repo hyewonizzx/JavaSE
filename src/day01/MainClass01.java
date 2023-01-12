946f3bc15a3e768ee4c4d0024a8e781ee7f04139
ITSC <ITSC@DESKTOP-ONE4R81>
2023-01-12 21:51:31 +0900

    첫 커밋

---
+++ src/day01/MainClass01.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class MainClass01 {
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	
+	}
+	
+	
+}
---
+++ src/day01/MainClass02.java
@@ -0,0 +1,22 @@
+package day01;
+
+/*
+ * 주석 - 컴퓨터가 인식하지 않는 영역
+ * 여러줄 주석입니다.
+ */
+
+// 자바 코드 시작 class입니다. (한줄 주석)
+// 자바 코드는 class 예약어 {} 안에 정의
+public class MainClass02 {
+	/**
+	 * JavaDoc 주석 (alt + shift + j): 공식적인 주소 (main 누르면 뜸)
+	 * 
+	 * 메인 메소드 - 자바 처음 실행되는 메소드
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Console 출력 명령어
+		System.out.println("안녕하세요");
+	}
+	
+}
